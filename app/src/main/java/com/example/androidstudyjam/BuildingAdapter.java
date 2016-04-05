21fefe23a9b655f8d12a158a80b06bd5d121c79e
Anastasiya <inth333@gmail.com>
2016-04-05 03:02:25 +0300

    s

---
+++ app/src/main/java/com/example/androidstudyjam/BuildingAdapter.java
@@ -0,0 +1,62 @@
+package com.example.androidstudyjam;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+/**
+ * Created by Дом on 04.04.2016.
+ */
+public class BuildingAdapter extends RecyclerView.Adapter<BuildingAdapter.ViewHolder> {
+
+    private Context mContext;
+
+    private String mDataSet[];
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView mImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.mImageView = (ImageView) itemView.findViewById(R.id.image_view_buildigs);
+        }
+    }
+
+    public BuildingAdapter(Context context, String[] mDataSet) {
+        this.mContext = context;
+        this.mDataSet = mDataSet;
+    }
+
+    @Override
+    public BuildingAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.image_view, parent, false);
+        ViewHolder mViewHolder = new ViewHolder(view);
+        return mViewHolder;
+
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(BuildingAdapter.ViewHolder holder, int position) {
+        Picasso.with(mContext)
+                .load(mDataSet[position])
+                .into(holder.mImageView);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+}
---
+++ app/src/main/java/com/example/androidstudyjam/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.androidstudyjam;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false));
+
+        Resources mRes = getResources();
+        String[] buildings = mRes.getStringArray(R.array.buildings_arr);
+
+        mAdapter = new BuildingAdapter(getBaseContext(), buildings);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
